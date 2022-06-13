e0f450ea907549fc71d7c586068c89d092e0a801
Kazuaki Shioda <s.kazuaki0930@gmail.com>
2022-06-13 15:56:43 +0900

    reviced

--- src/gcd.c
+++ src/gcd.c
@@ -11,7 +11,7 @@ unsigned int Euclidean_gcd_rec(unsigned int x, unsigned int y){
 unsigned int Euclidean_gcd_itr(unsigned int x, unsigned int y){
     while(x != 0){
         unsigned int z = x;
-        x = y %= x;
+        x = y % x;
         y = z;
     }
 
