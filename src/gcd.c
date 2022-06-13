74ca306181f1659ce0b0bd0f160ffa0fb0691544
Kouta Someya <40230524+larcyK@users.noreply.github.com>
2022-06-14 02:10:33 +0900

    revised2

--- src/gcd.c
+++ src/gcd.c
@@ -7,21 +7,51 @@ unsigned int Euclidean_gcd_rec(unsigned int x, unsigned int y){
   return Euclidean_gcd_rec(y % x, x);
 }
 
-/*
 unsigned int Euclidean_gcd_itr(unsigned int x, unsigned int y){
+  while(x != 0) {
+    unsigned int z = x;
+    x = y % x;
+    y = z;
+  }
+  return y;
 }
 
 unsigned int binary_gcd_rec(unsigned int x, unsigned int y){
+  if(x == 0) return y;
+  if(y == 0) return x;
+  if((x & 1) == 0)
+    if((y & 1) == 0) return binary_gcd_rec(x >> 1, y >> 1) << 1;
+    else return binary_gcd_rec(x >> 1, y);
+  else
+    if((y & 1) == 0) return binary_gcd_rec(x, y >> 1);
+    else if(y >= x) return binary_gcd_rec(x, (y-x) >> 1);
+  return binary_gcd_rec((x-y) >> 1, y);
 }
 
 unsigned int binary_gcd_itr(unsigned int x, unsigned int y){
+  unsigned int cor = 1, x_ = 0, y_ = 0;
+  if(x == 0) return y;
+  while(y != 0){
+    x_ = (x & 1);
+    y_ = (y & 1);
+    if((x_ | y_) == 0) cor <<= 1;
+    if(x_ == 0) x >>= 1;
+    if(y_ == 0) y >>= 1;
+    if(x_ & y_) {
+      if(x > y) {
+        unsigned int z = x;
+        x = y;
+        y = z;
+      }
+      y = (y - x) >> 1;
+    }
+  }
+  return x * cor;
 }
-*/
 
 
 int main(){
   unsigned int i, j, c;
-
 
   c = 0;
   for(i = 1; i <= n; i++){
