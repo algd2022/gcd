db81e91f818a1cd32f948b3cf93499d7bb3fb79d
Daiki Nagai <nagai.d.ab@m.titech.ac.jp>
2022-06-16 21:37:03 +0900

    revised2

--- src/gcd.c
+++ src/gcd.c
@@ -3,9 +3,13 @@
 const int n = 1000;
 
 unsigned int Euclidean_gcd_rec(unsigned int x, unsigned int y){
-  if(x==0) return y;
-  return Euclidean_gcd_rec(y % x, x);
-} 
+  while(x != 0){
+    unsigned int z = x;
+    x = y % x;
+    y = z;
+ }
+return y;
+}
 
 /*
 unsigned int Euclidean_gcd_itr(unsigned int x, unsigned int y){
