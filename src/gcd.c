fd6fa26f290b0123080a2bb51d7ca4c60c3cf4f6
Kosei Oishi <oishi.k.ah@m.titech.ac.jp>
2022-06-19 12:41:33 +0900

    revised

--- src/gcd.c
+++ src/gcd.c
@@ -6,6 +6,7 @@ unsigned int Euclidean_gcd_rec(unsigned int x, unsigned int y){
   if (x == 0) return y;
   return Euclidean_gcd_rec(y % x, x);
 }
+
 /*
 unsigned int Euclidean_gcd_itr(unsigned int x, unsigned int y){
 }
