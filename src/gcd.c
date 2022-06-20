88582402d29c6e32b66a45dfd66d4bfdb2313bd0
calliope <calliope183@gmail.com>
2022-06-20 10:06:07 +0000

    implement ` Euclidean_gcd_rec`

--- src/gcd.c
+++ src/gcd.c
@@ -1,36 +1,26 @@
 #include <stdio.h>
 
-const int n = 1000;
+const unsigned int n = 1000;
 
-unsigned int Euclidean_gcd_rec(unsigned int x, unsigned int y){
+unsigned int Euclidean_gcd_rec(unsigned int x, unsigned int y) {
+  return x == 0 ? y : Euclidean_gcd_rec(y % x, x);
 }
 
-/*
-unsigned int Euclidean_gcd_itr(unsigned int x, unsigned int y){
-}
-
-unsigned int binary_gcd_rec(unsigned int x, unsigned int y){
-}
-
-unsigned int binary_gcd_itr(unsigned int x, unsigned int y){
-}
-*/
-
-
-int main(){
+int main() {
   unsigned int i, j, c;
 
-
   c = 0;
-  for(i = 1; i <= n; i++){
-    for(j = i + 1; j <= n; j++){
-      if(Euclidean_gcd_rec(i, j) == 1){
+  for(i = 1; i <= n; i++) {
+    for(j = i + 1; j <= n; j++) {
+      // printf("i=%u, j=%u, gcd=%u\n", i, j, Euclidean_gcd_rec(i, j));
+      if(Euclidean_gcd_rec(i, j) == 1) {
         c++;
       }
     }
   }
   c = 2 * c + 1;
 
+  // 切り捨てないように`c`を`double`へキャスト
   printf("%f\n", (double) c / (n * n));
   return 0;
 }
