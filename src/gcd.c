469e27435ac0e5880670b64ebb54eea13b77de84
hishimochi <jade346producer@gmail.com>
2022-06-21 11:07:49 +0900

    reviced

--- src/gcd.c
+++ src/gcd.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
-const int n = 1000;
+const int n = 10000;
 
 unsigned int Euclidean_gcd_rec(unsigned int x, unsigned int y){
-    return 0;
+    if(x == 0) return y;
+    return Euclidean_gcd_rec(y % x, x);
 }
 
 /*
